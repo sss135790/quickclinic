2594571b8a4595be32f311328df761d1c1309925
sss135790 <123950334+sss135790@users.noreply.github.com>
2024-09-28 12:27:57 +0530

    Update navbar.js

--- frontend/src/component/layout/header/navbar/navbar.js
+++ frontend/src/component/layout/header/navbar/navbar.js
@@ -3,12 +3,17 @@ import { Button, Offcanvas, ListGroup } from 'react-bootstrap';
 import { House, InfoCircle, BoxArrowInRight, BoxArrowLeft, ChatDots, Calendar } from 'react-bootstrap-icons';
 import './navbar.css';
 import { useNavigate } from 'react-router-dom';
+import { FaHistory } from 'react-icons/fa';
+import { MdCancel } from "react-icons/md";
+import { RxUpdate } from "react-icons/rx";
+import ConfirmationModal from '../../../common/ConfirmationModal';
 
 const SideNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [authState, setAuthState] = useState(false);
+  const [confirmationModal, setConfirmationModal] = useState(null)
   const [role, setRole] = useState(null);
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
   const [id, setId] = useState('');
 
   useEffect(() => {
@@ -29,14 +34,30 @@ const SideNavbar = () => {
 
   const login = () => {
     navigate('/user/login');
+    setIsOpen(false);
   };
 
   const logout = () => {
+    setIsOpen(false);
+
+    setConfirmationModal({
+      text1: "Are you sure?",
+      text2: "You will be logged out of your account.",
+      btn1Text: "Logout",
+      btn2Text: "Cancel",
+      btn1Handler: ()=> LOGOUT(),  
+      btn2Handler: ()=> setConfirmationModal(null), 
+  })
+  };
+
+  const LOGOUT=()=>{
     localStorage.removeItem('authState');
+    setConfirmationModal(null); 
     setAuthState(false);
     setRole(null);
     navigate('/user/home');
-  };
+    setIsOpen(false);
+  }
 
   const handleUpdate = () => {
     if (role === 'patient') {
@@ -44,20 +65,45 @@ const SideNavbar = () => {
     } else if (role === 'doctor') {
       navigate(`/doctor/${id}/update_doctor`);
     }
+    setIsOpen(false); 
   };
 
   const navigateToChats = () => {
     navigate(`/user/${id}/chats`);
+    setIsOpen(false);
   };
 
   const navigateToSchedule = () => {
     if (role === 'doctor') {
       navigate(`/doctor/${id}/update_schedule`);
     }
+    setIsOpen(false);
+  };
+
+  const navigateToAppointment = () => {
+    if (role === 'patient') {
+      navigate(`/patient/dashboard/${id}/appointment`);
+    }
+    setIsOpen(false);
+  };
+
+  const navigateToHistory = () => {
+    if (role === 'patient') {
+      navigate(`/patient/dashboard/${id}/history`);
+    }
+    setIsOpen(false);
+  };
+
+  const navigateToCancel = () => {
+    if (role === 'patient') {
+      navigate(`/patient/dashboard/${id}/cancel/postpone`);
+    }
+    setIsOpen(false);
   };
 
   const navigateToAbout = () => {
     navigate('/user/about'); // Navigate to the about page
+    setIsOpen(false);
   };
 
   return (
@@ -81,7 +127,8 @@ const SideNavbar = () => {
 
             {authState && (
               <ListGroup.Item action onClick={handleUpdate} className="list-item">
-                {role === 'patient' ? 'Update Patient Data' : 'Update Doctor Data'}
+                
+                <RxUpdate /> {role === 'patient' ? 'Update Patient Data' : 'Update Doctor Data'}
               </ListGroup.Item>
             )}
 
@@ -96,6 +143,25 @@ const SideNavbar = () => {
                 <Calendar className="me-2" /> Schedule
               </ListGroup.Item>
             )}
+
+            {authState && role === 'patient' && (
+              <ListGroup.Item action onClick={navigateToAppointment} className="list-item">
+                <Calendar className="me-2" /> Book An Appointment 
+              </ListGroup.Item>
+            )}
+
+          {authState && role === 'patient' && (
+              <ListGroup.Item action onClick={navigateToCancel} className="list-item">
+                <MdCancel className="me-2" /> Cancel An Appointment 
+              </ListGroup.Item>
+            )} 
+
+          {authState && role === 'patient' && (
+              <ListGroup.Item action onClick={navigateToHistory} className="list-item">
+                < FaHistory  className="me-2" /> History
+              </ListGroup.Item>
+            )}
+
           </ListGroup>
 
           <Button
@@ -108,6 +174,7 @@ const SideNavbar = () => {
           </Button>
         </Offcanvas.Body>
       </Offcanvas>
+      {confirmationModal && <ConfirmationModal modalData={confirmationModal}/>}
     </>
   );
 };
